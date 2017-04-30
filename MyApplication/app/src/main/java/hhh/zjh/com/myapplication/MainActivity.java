49b5953ac8ccec29aa4249447fd99c9db0d362ac
276336290@qq.com <276336290@qq.com>
2017-04-30 18:06:41 +0800

    计算器byZJH

--- MyApplication/app/src/main/java/hhh/zjh/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/hhh/zjh/com/myapplication/MainActivity.java
@@ -1,13 +1,157 @@
 package hhh.zjh.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+ /**
+  * 数字
+  */
+private Button num0;
+    private Button num1;
+    private Button num2;
+    private Button num3;
+    private Button num4;
+    private Button num5;
+    private Button num6;
+    private Button num7;
+    private Button num8;
+    private Button num9;
+    /**
+     * 运算符
+     */
+    private Button plus_btn;
+    private Button subtract_btn;
+    private Button multiply_btn;
+    private Button divide_btn;
+    private Button equal_btn;
+    /**
+     * 其他
+     */
+    private Button dot_btn;
+    private Button percent_btn;
+    private Button delete_btn;
+    private Button ac_btn;
+    /**
+     * 结果
+     */
+    private EditText mResultText;
+    /**
+     * 已经输入的字符
+     */
+    private String existedText = "";
+    /**
+     * 是否计算过
+     */
+    private boolean isCounted = false;
+    /**
+     * 以负号开头，且运算符不是是减号
+     * 例如：-21×2
+     */
+    private boolean startWithOperator = false;
+    /**
+     * 以负号开头，且运算符是减号
+     * 例如：-21-2
+     */
+    private boolean startWithSubtract = false;
+    /**
+     * 不以负号开头，且包含运算符
+     * 例如：21-2
+     */
+    private boolean noStartWithOperator = false;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();  /**初始视图（界面控件）*/
+        initEvent();/**初始化新事件对象的属性*/
     }
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        /**
+         * 数字
+         */
+        num0 = (Button) findViewById(R.id.num_zero);
+        num1 = (Button) findViewById(R.id.num_one);
+        num2 = (Button) findViewById(R.id.num_two);
+        num3 = (Button) findViewById(R.id.num_three);
+        num4 = (Button) findViewById(R.id.num_four);
+        num5 = (Button) findViewById(R.id.num_five);
+        num6 = (Button) findViewById(R.id.num_six);
+        num7 = (Button) findViewById(R.id.num_seven);
+        num8 = (Button) findViewById(R.id.num_eight);
+        num9 = (Button) findViewById(R.id.num_nine);
+        /**
+         * 运算符
+         */
+        plus_btn = (Button) findViewById(R.id.plus_btn);
+        subtract_btn = (Button) findViewById(R.id.subtract_btn);
+        multiply_btn = (Button) findViewById(R.id.multiply_btn);
+        divide_btn = (Button) findViewById(R.id.divide_btn);
+        equal_btn = (Button) findViewById(R.id.equal_btn);
+        /**
+         * 其他
+         */
+        dot_btn = (Button) findViewById(R.id.dot_btn);
+        percent_btn = (Button) findViewById(R.id.percent_btn);
+        delete_btn = (Button) findViewById(R.id.delete_btn);
+        ac_btn = (Button) findViewById(R.id.ac_btn);
+        /**
+         * 结果
+         */
+        mResultText = (EditText) findViewById(R.id.result_text);
+        /**
+         * 已经输入的字符
+         */
+        existedText = mResultText.getText().toString();
+
+    }
+    /**
+     * 初始化事件
+     */
+    private void initEvent() {
+        num0.setOnClickListener(this);
+        num1.setOnClickListener(this);
+        num2.setOnClickListener(this);
+        num3.setOnClickListener(this);
+        num4.setOnClickListener(this);
+        num5.setOnClickListener(this);
+        num6.setOnClickListener(this);
+        num7.setOnClickListener(this);
+        num8.setOnClickListener(this);
+        num9.setOnClickListener(this);
+
+        plus_btn.setOnClickListener(this);
+        subtract_btn.setOnClickListener(this);
+        multiply_btn.setOnClickListener(this);
+        divide_btn.setOnClickListener(this);
+        equal_btn.setOnClickListener(this);
+
+        dot_btn.setOnClickListener(this);
+        percent_btn.setOnClickListener(this);
+        delete_btn.setOnClickListener(this);
+        ac_btn.setOnClickListener(this);
+    }
+    /**
+     * 点击事件
+     * @param v  点击的控件
+     */
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
+
+
+
