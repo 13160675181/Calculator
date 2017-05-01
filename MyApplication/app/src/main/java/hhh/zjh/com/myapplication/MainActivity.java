ca4295c7ff499845b3219da18fbd31ee87ee41be
276336290@qq.com <276336290@qq.com>
2017-05-01 11:13:49 +0800

    计算器byZJH

--- MyApplication/app/src/main/java/hhh/zjh/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/hhh/zjh/com/myapplication/MainActivity.java
@@ -2,15 +2,16 @@ package hhh.zjh.com.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
- /**
-  * 数字
-  */
-private Button num0;
+    /**
+     * 数字
+     */
+    private Button num0;
     private Button num1;
     private Button num2;
     private Button num3;
@@ -64,8 +65,6 @@ private Button num0;
     private boolean noStartWithOperator = false;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +73,7 @@ private Button num0;
         initView();  /**初始视图（界面控件）*/
         initEvent();/**初始化新事件对象的属性*/
     }
+
     /**
      * 初始化控件
      */
@@ -116,6 +116,7 @@ private Button num0;
         existedText = mResultText.getText().toString();
 
     }
+
     /**
      * 初始化事件
      */
@@ -142,15 +143,452 @@ private Button num0;
         delete_btn.setOnClickListener(this);
         ac_btn.setOnClickListener(this);
     }
+
     /**
      * 点击事件
-     * @param v  点击的控件
+     *
+     * @param v 点击的控件
      */
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()) {
+            /**
+             * 数字
+             */
+            case R.id.num_zero:
+                existedText = isOverRange(existedText, "0");
+                break;
+            case R.id.num_one:
+                existedText = isOverRange(existedText, "1");
+                break;
+            case R.id.num_two:
+                existedText = isOverRange(existedText, "2");
+                break;
+            case R.id.num_three:
+                existedText = isOverRange(existedText, "3");
+                break;
+            case R.id.num_four:
+                existedText = isOverRange(existedText, "4");
+                break;
+            case R.id.num_five:
+                existedText = isOverRange(existedText, "5");
+                break;
+            case R.id.num_six:
+                existedText = isOverRange(existedText, "6");
+                break;
+            case R.id.num_seven:
+                existedText = isOverRange(existedText, "7");
+                break;
+            case R.id.num_eight:
+                existedText = isOverRange(existedText, "8");
+                break;
+            case R.id.num_nine:
+                existedText = isOverRange(existedText, "9");
+                break;
+            /**
+             * 运算符
+             */
+            case R.id.plus_btn:
+                /**
+                 * 判断已有的字符是否是科学计数
+                 * 是 置零
+                 * 否 进行下一步
+                 *
+                 * 判断表达式是否可以进行计算
+                 * 是 先计算再添加字符
+                 * 否 添加字符
+                 *
+                 * 判断计算后的字符是否是 error
+                 * 是 置零
+                 * 否 添加运算符
+                 */
+                if (!existedText.contains("e")) {
+
+                    if (judgeExpression()) {
+                        existedText = getResult();
+                        if (existedText.equals("error")) {
+
+                        } else {
+                            existedText += "+";
+                        }
+                    } else {
+
+                        if (isCounted) {
+                            isCounted = false;
+                        }
+
+                        if ((existedText.substring(existedText.length() - 1)).equals("-")) {
+                            existedText = existedText.replace("-", "+");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("×")) {
+                            existedText = existedText.replace("×", "+");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("÷")) {
+                            existedText = existedText.replace("÷", "+");
+                        } else if (!(existedText.substring(existedText.length() - 1)).equals("+")) {
+                            existedText += "+";
+                        }
+                    }
+                } else {
+                    existedText = "0";
+                }
+
+                break;
+            case R.id.subtract_btn:
+
+                if (!existedText.contains("e")) {
+                    if (judgeExpression()) {
+                        existedText = getResult();
+                        if (existedText.equals("error")) {
+
+                        } else {
+                            existedText += "-";
+                        }
+                    } else {
+
+                        if (isCounted) {
+                            isCounted = false;
+                        }
+
+                        if ((existedText.substring(existedText.length() - 1)).equals("+")) {
+//                    Log.d("Anonymous", "onClick: " + "进入减法方法");
+                            existedText = existedText.replace("+", "-");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("×")) {
+                            existedText = existedText.replace("×", "-");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("÷")) {
+                            existedText = existedText.replace("÷", "-");
+                        } else if (!(existedText.substring(existedText.length() - 1)).equals("-")) {
+                            existedText += "-";
+                        }
+                    }
+                } else {
+                    existedText = "0";
+                }
+                break;
+            case R.id.multiply_btn:
+
+                if (!existedText.contains("e")) {
+                    if (judgeExpression()) {
+                        existedText = getResult();
+                        if (existedText.equals("error")) {
+
+                        } else {
+                            existedText += "×";
+                        }
+
+                    } else {
+
+                        if (isCounted) {
+                            isCounted = false;
+                        }
+
+                        if ((existedText.substring(existedText.length() - 1)).equals("+")) {
+                            existedText = existedText.replace("+", "×");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("-")) {
+                            existedText = existedText.replace("-", "×");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("÷")) {
+                            existedText = existedText.replace("÷", "×");
+                        } else if (!(existedText.substring(existedText.length() - 1)).equals("×")) {
+                            existedText += "×";
+                        }
+                    }
+                } else {
+                    existedText = "0";
+                }
+                break;
+            case R.id.divide_btn:
+
+                if (!existedText.contains("e")) {
+                    if (judgeExpression()) {
+                        existedText = getResult();
+                        if (existedText.equals("error")) {
+
+                        } else {
+                            existedText += "÷";
+                        }
+
+                    } else {
+
+                        if (isCounted) {
+                            isCounted = false;
+                        }
+
+                        if ((existedText.substring(existedText.length() - 1)).equals("+")) {
+                            existedText = existedText.replace("+", "÷");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("-")) {
+                            existedText = existedText.replace("-", "÷");
+                        } else if ((existedText.substring(existedText.length() - 1)).equals("×")) {
+                            existedText = existedText.replace("×", "÷");
+                        } else if (!(existedText.substring(existedText.length() - 1)).equals("÷")) {
+                            existedText += "÷";
+                        }
+                    }
+                } else {
+                    existedText = "0";
+                }
+                break;
+            case R.id.equal_btn:
+                existedText = getResult();
+                isCounted = true;
+                break;
+            /**
+             * 其他
+             */
+            case R.id.dot_btn:
+                /**
+                 * 判断是否运算过
+                 * 否
+                 *   判断是否有运算符，有 判断运算符之后的数字，无 判断整个数字
+                 *   判断数字是否过长，是则不能添加小数点，否则可以添加
+                 *   判断已经存在的数字里是否有小数点
+                 * 是
+                 *   字符串置为 0.
+                 */
+                if (!isCounted) {
+
+                    if (existedText.contains("+") || existedText.contains("-") ||
+                            existedText.contains("×") || existedText.contains("÷")) {
+
+                        String param1 = null;
+                        String param2 = null;
+
+                        if (existedText.contains("+")) {
+                            param1 = existedText.substring(0, existedText.indexOf("+"));
+                            param2 = existedText.substring(existedText.indexOf("+") + 1);
+                        } else if (existedText.contains("-")) {
+                            param1 = existedText.substring(0, existedText.indexOf("-"));
+                            param2 = existedText.substring(existedText.indexOf("-") + 1);
+                        } else if (existedText.contains("×")) {
+                            param1 = existedText.substring(0, existedText.indexOf("×"));
+                            param2 = existedText.substring(existedText.indexOf("×") + 1);
+                        } else if (existedText.contains("÷")) {
+                            param1 = existedText.substring(0, existedText.indexOf("÷"));
+                            param2 = existedText.substring(existedText.indexOf("÷") + 1);
+                        }
+                        Log.d("Anonymous param1", param1);
+                        Log.d("Anonymous param2", param2);
+
+                        boolean isContainedDot = param2.contains(".");
+                        if (param2.length() >= 9) {
+
+                        } else if (!isContainedDot) {
+                            if (param2.equals("")) {
+                                existedText += "0.";
+                            } else {
+                                existedText += ".";
+                            }
+                        } else {
+                            return;
+                        }
+                    } else {
+                        boolean isContainedDot = existedText.contains(".");
+                        if (existedText.length() >= 9) {
+
+                        } else if (!isContainedDot) {
+                            existedText += ".";
+                        } else {
+                            return;
+                        }
+                    }
+                    isCounted = false;
+
+                } else {
+                    existedText = "0.";
+                    isCounted = false;
+                }
+
+
+                break;
+            case R.id.percent_btn:
+                /**
+                 * 判断数字是否有运算符
+                 * 是 不做任何操作
+                 * 否 进行下一步
+                 *
+                 * 判断数字是否是 0
+                 * 是 不做任何操作
+                 * 否 进行除百
+                 *
+                 * 将字符串转换成double类型，进行运算后，再转换成String型
+                 */
+                if (existedText.equals("error")) {
+
+                } else {
+
+                    //getCondition();
+
+                    if (startWithOperator || startWithSubtract || noStartWithOperator) {
+
+                    } else {
+                        if (existedText.equals("0")) {
+                            return;
+                        } else {
+                            double temp = Double.parseDouble(existedText);
+                            temp = temp / 100;
+                            existedText = String.valueOf(temp);
+                        }
+                    }
+                }
+                break;
+            case R.id.delete_btn:
+                /**
+                 * 字符串长度大于 0 时才截取字符串
+                 * 如果长度为 1，则直接把字符串设置为 0
+                 */
+                if (existedText.equals("error")) {
+                    existedText = "0";
+                } else if (existedText.length() > 0) {
+                    if (existedText.length() == 1) {
+                        existedText = "0";
+                    } else {
+                        existedText = existedText.substring(0, existedText.length() - 1);
+                    }
+                }
+                break;
+            case R.id.ac_btn:
+                existedText = "0";
+                break;
+        }
+        /**
+         * 设置显示
+         */
+        mResultText.setText(existedText);
     }
 
+    /**
+     * 先判断是否按过等于号
+     * 是 按数字则显示当前数字
+     * 否 在已有的表达式后添加字符
+     * <p>
+     * 判断数字是否就是一个 0
+     * 是 把字符串设置为空字符串。
+     * 1、打开界面没有运算过的时候，AC键归零或删除完归零的时候，会显示一个 0
+     * 2、当数字是 0 的时候，设置成空字符串，再按 0 ，数字会还是 0，不然可以按出 000 这种数字
+     * 否 添加按下的键的字符
+     * <p>
+     * 判断数字是否包含小数点
+     * 是 数字不能超过十位
+     * 否 数字不能超过九位
+     * <p>
+     * 进行上面的判断后，再判断数字是否超过长度限制
+     * 超过不做任何操作
+     * 没超过可以再添数字
+     */
+    private String isOverRange(String existedText, String s) {
+        /**
+         * 判断是否计算过
+         */
+        if (!isCounted) {
+            /**
+             * 判断是否是科学计数
+             * 是 文本置零
+             */
+            if (existedText.contains("e")) {
+                existedText = "0";
+            }
+            /**
+             * 判断是否只有一个 0
+             * 是 文本清空
+             */
+            if (existedText.equals("0")) {
+                existedText = "";
+            }
+            /**
+             * 判断是否有运算符
+             * 是 判断第二个数字
+             * 否 判断整个字符串
+             */
+            if (existedText.contains("+") || existedText.contains("-") ||
+                    existedText.contains("×") || existedText.contains("÷")) {
+                /**
+                 * 包括运算符时 两个数字 判断第二个数字
+                 * 两个参数
+                 */
+                String param2 = null;
+                if (existedText.contains("+")) {
+                    param2 = existedText.substring(existedText.indexOf("+") + 1);
+                } else if (existedText.contains("-")) {
+                    param2 = existedText.substring(existedText.indexOf("-") + 1);
+                } else if (existedText.contains("×")) {
+                    param2 = existedText.substring(existedText.indexOf("×") + 1);
+                } else if (existedText.contains("÷")) {
+                    param2 = existedText.substring(existedText.indexOf("÷") + 1);
+                }
+
+//            Log.d("Anonymous param1",param1);
+//            Log.d("Anonymous param2",param2);
+                if (existedText.substring(existedText.length() - 1).equals("+") ||
+                        existedText.substring(existedText.length() - 1).equals("-") ||
+                        existedText.substring(existedText.length() - 1).equals("×") ||
+                        existedText.substring(existedText.length() - 1).equals("÷")) {
+                    existedText += s;
+                } else {
+                    if (param2.contains(".")) {
+                        if (param2.length() >= 10) {
+
+                        } else {
+                            existedText += s;
+                        }
+                    } else {
+                        if (param2.length() >= 9) {
+
+                        } else {
+                            existedText += s;
+                        }
+                    }
+                }
+            } else {
+                /**
+                 * 不包括运算符时 一个数字
+                 */
+                if (existedText.contains(".")) {
+                    if (existedText.length() >= 10) {
+
+                    } else {
+                        existedText += s;
+                    }
+                } else {
+                    if (existedText.length() >= 9) {
+
+                    } else {
+                        existedText += s;
+                    }
+                }
+            }
+
+            isCounted = false;
+
+        } else {
+
+            existedText = s;
+            isCounted = false;
+
+        }
+
+
+        return existedText;
+    }
+
+    /**
+     * 判断表达式
+     * <p>
+     * 为了按等号是否进行运算
+     * 以及出现两个运算符（第一个参数如果为负数的符号不计）先进行运算再添加运算符
+     */
+    private boolean judgeExpression() {
+        return false;
+    }
+
+    /**
+     * 进行运算，得到结果
+     * @return  返回结果
+     */
+    private String getResult() {
+        /**
+         * 结果
+         */
+        String tempResult = null;
+        return tempResult;
+    }
 }
 
 
