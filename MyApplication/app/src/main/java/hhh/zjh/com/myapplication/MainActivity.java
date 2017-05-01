4431079a4b48690af14c750ae4b5d8435bd80aa8
276336290@qq.com <276336290@qq.com>
2017-05-01 12:38:31 +0800

    计算器byZJH

--- MyApplication/app/src/main/java/hhh/zjh/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/hhh/zjh/com/myapplication/MainActivity.java
@@ -414,7 +414,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 } else {
 
-                    //getCondition();
+                    getCondition();
 
                     if (startWithOperator || startWithSubtract || noStartWithOperator) {
 
@@ -575,8 +575,88 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * 以及出现两个运算符（第一个参数如果为负数的符号不计）先进行运算再添加运算符
      */
     private boolean judgeExpression() {
+        getCondition();
+
+        String tempParam2 = null;
+
+        if ( startWithOperator || noStartWithOperator || startWithSubtract) {
+
+            if (existedText.contains("+")) {
+                /**
+                 * 先获取第二个参数
+                 */
+                tempParam2 = existedText.substring(existedText.indexOf("+") + 1);
+                /**
+                 * 如果第二个参数为空，表达式不成立
+                 */
+                if (tempParam2.equals("")) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else if (existedText.contains("×")) {
+
+                tempParam2 = existedText.substring(existedText.indexOf("×") + 1);
+
+                if (tempParam2.equals("")) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+            } else if (existedText.contains("÷")) {
+
+                tempParam2 = existedText.substring(existedText.indexOf("÷") + 1);
+
+                if (tempParam2.equals("")) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+            } else if (existedText.contains("-")) {
+
+                /**
+                 * 这里是以最后一个 - 号为分隔去取出两个参数
+                 * 进到这个方法，必须满足有运算公式
+                 * 而又避免了第一个参数是负数的情况
+                 */
+                tempParam2 = existedText.substring(existedText.lastIndexOf("-") + 1);
+
+                if (tempParam2.equals("")) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+            }
+        }
         return false;
     }
+
+    /**
+     * 取得判断条件
+     */
+    private void getCondition() {
+        /**
+         * 以负号开头，且运算符不是是减号
+         * 例如：-21×2
+         */
+        startWithOperator = existedText.startsWith("-") && ( existedText.contains("+") ||
+                existedText.contains("×") || existedText.contains("÷") );
+        /**
+         * 以负号开头，且运算符是减号
+         * 例如：-21-2
+         */
+        startWithSubtract = existedText.startsWith("-") && ( existedText.lastIndexOf("-") != 0 );
+        /**
+         * 不以负号开头，且包含运算符
+         * 例如：21-2
+         */
+        noStartWithOperator = !existedText.startsWith("-") && ( existedText.contains("+") ||
+                existedText.contains("-") || existedText.contains("×") || existedText.contains("÷"));
+    }
+
 
     /**
      * 进行运算，得到结果
