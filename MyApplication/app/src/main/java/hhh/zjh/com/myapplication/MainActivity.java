fdabecbea67150e064e271ea57e7e51ad21b408f
276336290@qq.com <276336290@qq.com>
2017-05-01 23:00:28 +0800

    计算器byZJH

--- MyApplication/app/src/main/java/hhh/zjh/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/hhh/zjh/com/myapplication/MainActivity.java
@@ -663,12 +663,142 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @return  返回结果
      */
     private String getResult() {
+
         /**
          * 结果
          */
         String tempResult = null;
+        /**
+         * 两个String类型的参数
+         */
+        String param1 = null;
+        String param2 = null;
+        /**
+         * 转换后的两个double类型的参数
+         */
+        double arg1 = 0;
+        double arg2 = 0;
+        double result = 0;
+
+        getCondition();
+
+        /**
+         * 如果有运算符，则进行运算
+         * 没有运算符，则把已经存在的数据再传出去
+         */
+        if ( startWithOperator || noStartWithOperator || startWithSubtract) {
+
+            if (existedText.contains("+")) {
+                /**
+                 * 先获取两个参数
+                 */
+                param1 = existedText.substring(0, existedText.indexOf("+"));
+                param2 = existedText.substring(existedText.indexOf("+") + 1);
+                /**
+                 * 如果第二个参数为空，则还是显示当前字符
+                 */
+                if (param2.equals("")){
+                    tempResult = existedText;
+                } else {
+                    /**
+                     * 转换String为Double
+                     * 计算后再转换成String类型
+                     * 进行正则表达式处理
+                     */
+                    arg1 = Double.parseDouble(param1);
+                    arg2 = Double.parseDouble(param2);
+                    result = arg1 + arg2;
+                    tempResult = String.format("%f", result);
+                    tempResult = subZeroAndDot(tempResult);
+                }
+
+
+            } else if (existedText.contains("×")) {
+
+                param1 = existedText.substring(0, existedText.indexOf("×"));
+                param2 = existedText.substring(existedText.indexOf("×") + 1);
+
+                if (param2.equals("")){
+                    tempResult = existedText;
+                } else {
+                    arg1 = Double.parseDouble(param1);
+                    arg2 = Double.parseDouble(param2);
+                    result = arg1 * arg2;
+                    tempResult = String.format("%f", result);
+                    tempResult = subZeroAndDot(tempResult);
+                }
+
+            } else if (existedText.contains("÷")) {
+
+                param1 = existedText.substring(0, existedText.indexOf("÷"));
+                param2 = existedText.substring(existedText.indexOf("÷") + 1);
+
+                if (param2.equals("0")){
+                    tempResult = "error";
+                } else if (param2.equals("")){
+                    tempResult = existedText;
+                } else {
+                    arg1 = Double.parseDouble(param1);
+                    arg2 = Double.parseDouble(param2);
+                    result = arg1 / arg2;
+                    tempResult = String.format("%f", result);
+                    tempResult = subZeroAndDot(tempResult);
+                }
+
+            } else if (existedText.contains("-")) {
+
+                /**
+                 * 这里是以最后一个 - 号为分隔去取出两个参数
+                 * 进到这个方法，必须满足有运算公式
+                 * 而又避免了第一个参数是负数的情况
+                 */
+                param1 = existedText.substring(0, existedText.lastIndexOf("-"));
+                param2 = existedText.substring(existedText.lastIndexOf("-") + 1);
+
+                if (param2.equals("")){
+                    tempResult = existedText;
+                } else {
+                    arg1 = Double.parseDouble(param1);
+                    arg2 = Double.parseDouble(param2);
+                    result = arg1 - arg2;
+                    tempResult = String.format("%f", result);
+                    tempResult = subZeroAndDot(tempResult);
+                }
+
+            }
+            /**
+             * 如果数据长度大于等于10位，进行科学计数
+             *
+             * 如果有小数点，再判断小数点前是否有十个数字，有则进行科学计数
+             */
+            if (tempResult.length() >= 10) {
+                tempResult = String.format("%e", Double.parseDouble(tempResult));
+            } else if (tempResult.contains(".")) {
+                if (tempResult.substring(0, tempResult.indexOf(".")).length() >= 10) {
+                    tempResult = String.format("%e", Double.parseDouble(tempResult));
+                }
+            }
+        } else {
+            tempResult = existedText;
+        }
+
         return tempResult;
     }
+
+    /**
+     * 使用java正则表达式去掉多余的.与0
+     * @param s 传入的字符串
+     * @return 修改之后的字符串
+     */
+    public static String subZeroAndDot(String s){
+        if(s.indexOf(".") > 0){
+            s = s.replaceAll("0+?$", "");//去掉多余的0
+            s = s.replaceAll("[.]$", "");//如最后一位是.则去掉
+        }
+        return s;
+    }
+
+
 }
 
 
